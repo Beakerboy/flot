37a70c7bbf6ff54a8d650b725f00e5784f640e75
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-01-30 06:57:43 -0500

    Fixed jshint errors

--- jquery.flot.stack.js
+++ jquery.flot.stack.js
@@ -154,7 +154,7 @@ charts or filled areas).
                         newpoints[l + accumulateOffset] += bottom;
                         
                         i += ps;
-                    }
+                    };
 
                     fromgap = false;
                     
